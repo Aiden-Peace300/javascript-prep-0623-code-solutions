a62f057a3ae1c4d4ef006f4504fcd6078aa8e3f4
Aiden Peace <aidenpeace300@gmail.com>
2023-06-19 19:31:19 +0000

    Completed prep-html-script-tag

---
+++ prep-html-script-tag/main.js
@@ -0,0 +1,4 @@
+/* The prase "I have added a script file via the src attribute!" will be
+  outputted to the console
+  NOTE: this is why we use console.log();                            */
+console.log('I have added a script file via the src attribute!');
