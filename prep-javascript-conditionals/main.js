ee09e4c543e9828d700468f877c90b083d80fa19
Aiden Peace <aidenpeace300@gmail.com>
2023-06-28 01:19:46 +0000

    Updating gradeCalculator function in main.js

--- prep-javascript-conditionals/main.js
+++ prep-javascript-conditionals/main.js
@@ -30,13 +30,13 @@ const student2Score = 102;
 function gradeCalculator(score) {
   if (score < 60) {
     return 'F';
-  } else if (score >= 60 && score < 70) {
+  } else if (score < 70) {
     return 'D';
-  } else if (score >= 70 && score < 80) {
+  } else if (score < 80) {
     return 'C';
-  } else if (score >= 80 && score < 90) {
+  } else if (score < 90) {
     return 'B';
-  } else if (score >= 90 && score < 100) {
+  } else if (score < 100) {
     return 'A';
   } else {
     return 'A++';
